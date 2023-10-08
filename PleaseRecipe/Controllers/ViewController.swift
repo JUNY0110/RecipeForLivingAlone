47d913d2c6d375320bbbd3f193f05b3cd8881e63
Juny0110 <wnsdyds403@naver.com>
2023-10-08 17:33:15 +0900

    Chore: Project init setting
    - 프로젝트 초기 설정
    - AppDelegate 및 SceneDelegate의 불필요한 주석 제거
    - View, Controller, Model, ViewModel 폴더링
    - SceneDelegate 시작 파일을 Storyboard에서 ViewController로 변경

---
+++ PleaseRecipe/Controllers/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  PleaseRecipe
+//
+//  Created by 지준용 on 2023/10/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
---
+++ PleaseRecipe/Models/Model.swift
@@ -0,0 +1,8 @@
+//
+//  Model.swift
+//  PleaseRecipe
+//
+//  Created by 지준용 on 2023/10/08.
+//
+
+import Foundation
