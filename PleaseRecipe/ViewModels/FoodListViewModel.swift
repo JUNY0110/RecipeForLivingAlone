3b09f31079d0e2b50b0da247414c3afe3495f28d
Juny0110 <wnsdyds403@naver.com>
2023-10-22 19:31:33 +0900

    Refactor #13: Add FoodListViewModel
    - FoodListView에 대한 ViewModel 생성
    - 주기능: FoodData 배열 업데이트

---
+++ PleaseRecipe/ViewModels/FoodListViewModel.swift
@@ -0,0 +1,48 @@
+//
+//  ViewModel.swift
+//  PleaseRecipe
+//
+//  Created by 지준용 on 2023/10/08.
+//
+
+import UIKit
+
+final class FoodListViewModel {
+
+    // MARK: - Properties
+    
+    let networkManager: NetworkManager
+    
+    // MARK: Output
+    
+    private var foodDatum = [Food]() {
+        didSet {
+            onCompletedData(foodDatum)
+        }
+    }
+    
+    var onCompletedData: ([Food]) -> () = { _ in }
+    
+    // MARK: - Init
+    
+    init(networkManager: NetworkManager) {
+        self.networkManager = networkManager
+        
+        makeFoodDatum()
+    }
+    
+    // MARK: - Methods
+    
+    private func makeFoodDatum() {
+        for mainURL in APIEnvironment.mainURLs {
+            networkManager.makeFoodData(urlString: APIEnvironment.baseURL + mainURL) { result in
+                switch result {
+                case .success(let food):
+                    self.foodDatum.append(food)
+                case .failure(let error):
+                    debugPrint(error)
+                }
+            }
+        }
+    }
+}
