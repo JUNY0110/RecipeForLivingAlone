9b33ed0caa6d59f622ea72f14227c768556c1be8
Juny0110 <wnsdyds403@naver.com>
2023-11-01 01:11:23 +0900

    Resource #17: Modify UIFont extension

--- PleaseRecipe/Extensions/UIFont+extension.swift
+++ PleaseRecipe/Extensions/UIFont+extension.swift
@@ -9,10 +9,11 @@ import UIKit
 
 extension UIFont {
     static let h1 = UIFont.systemFont(ofSize: 22, weight: .bold)
-    static let h2 = UIFont.systemFont(ofSize: 18, weight: .semibold)
-    static let h3 = UIFont.systemFont(ofSize: 24, weight: .bold)
+    static let h2 = UIFont.systemFont(ofSize: 20, weight: .bold)
+    static let h3 = UIFont.systemFont(ofSize: 18, weight: .semibold)
     
     static let b1 = UIFont.systemFont(ofSize: 18, weight: .medium)
     static let b2 = UIFont.systemFont(ofSize: 16, weight: .regular)
-    static let b3 = UIFont.systemFont(ofSize: 14, weight: .regular)
+    static let b3 = UIFont.systemFont(ofSize: 16, weight: .semibold)
+    static let b4 = UIFont.systemFont(ofSize: 14, weight: .regular)
 }
